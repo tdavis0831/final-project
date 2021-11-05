a779a580e0ab8341d25156337c96c0a9f55d566a
Tasha Davis <tdavis0831@gmail.com>
2021-11-05 04:26:07 +0000

    api cleanup

--- api.py
+++ api.py
@@ -27,22 +27,29 @@ y = x['results']
   
 
 for i in range(len(y)): 
-      
+    list1 = []
+    list2= []
     # Print value corresponding to the 
     # 'name' key at the index of y 
-    print(y[i]['name'])
-    print(y[i]['formatted_address'])
+    list1.append(y[i]['name'])
+    print(list1)
+
+    list2.append(y[i]['formatted_address'])
+    print(list2)
+    # print(y[i]['formatted_address'])
+
+
     
-import requests
+# import requests
 
-url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name%2Crating%2Cformatted_phone_number&key={api_key}"
+# url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name%2Crating%2Cformatted_phone_number&key={api_key}"
 
-payload={}
-headers = {}
+# payload={}
+# headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# response = requests.request("GET", url, headers=headers, data=payload)
 
-print(response.text)
+# print(response.text)
 
 
 
