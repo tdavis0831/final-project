c3ba1c18dcd230bffb3c00affac561c64c97cc80
Tasha Davis <tdavis0831@gmail.com>
2021-11-13 20:31:15 +0000

    user form updated to require input

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-"""Server for movie questionaires"""
+"""Server for final project"""
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db
 import crud
